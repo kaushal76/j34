3676e2b4a94344b7b6a2527419bcba492a1c6dd7
sdeskpk <sdeskpk@10.74.3.118>
2015-08-14 12:17:31 +0100

    Redirect link fixed (when no authors for the paper)

--- components/com_confmgr/controllers/papers.php
+++ components/com_confmgr/controllers/papers.php
@@ -59,7 +59,7 @@ class ConfmgrControllerPapers extends JControllerAdmin
 			if (empty($authors->getAuthorsForPaper($return)))
 			{
 				$app->enqueueMessage(JText::_('COM_CONFMGR_CONTROLLER_AUTHORS_NEEDED', 'warning'));
-				$this->setRedirect(JRoute::_('index.php?option=com_confmgr&view=paper&layout=edit&id='.$return, false));
+				$this->setRedirect(JRoute::_('index.php?option=com_confmgr&view=author&layout=edit&id=0', false));
 			}
 			else
 			{
