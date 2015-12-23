2a10746addefd553e2fb1ec548ea3d07701e8c72
kaushal76 <kaushal.keraminiyage@gmail.com>
2015-12-23 18:14:37 +0000

    Fixed the login for J3.X

--- components/com_confmgt/controllers/loginform.php
+++ components/com_confmgt/controllers/loginform.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     2.5.8
+ * @version     3.4.1
  * @package     com_confmgt
  * @copyright   Copyright (C) 2015. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -8,13 +8,12 @@
  */
 // No direct access
 defined('_JEXEC') or die;
-
 require_once JPATH_COMPONENT.'/controller.php';
 
 /**
  * Login controller class.
  */
-class ConfmgtControllerLoginForm extends ConfmgtController
+class ConfmgtControllerLoginForm extends JControllerForm
 {
 
 	/**
@@ -36,66 +35,78 @@ class ConfmgtControllerLoginForm extends ConfmgtController
 		// Redirect to the edit screen.
 		$this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=loginform&layout=edit', false));
 	}
-	
+				
 	public function login()
 	{
 	   $app = JFactory::getApplication();
        $data = JFactory::getApplication()->input->get('jform', array(), 'array');
-	   
 	   $model = $this->getModel('LoginForm', 'ConfmgtModel');
    
-	   // Validate the posted data.
-		$form = $model->getForm();
-		if (!$form) {
-			JError::raiseError(500, $model->getError());
+			// Validate the posted data.
+		//@TODO remove the reference to JERROR and use PHP error handling
+		 
+		try
+		{
+			$form = $model->getForm();
+		}
+		catch (Exception $e)
+		{
+			throw new Exception($e->getMessage(),500);
 			return false;
 		}
-
+		
+	
 		// Validate the posted data.
 		$data = $model->validate($form, $data);
-
+	
 		// Check for errors.
-		
-		if ($data === false) {
+	
+		if ($data === false) 
+		{
 			// Get the validation messages.
 			$errors	= $model->getErrors();
-
+	
 			// Push up to three validation messages out to the user.
-			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
-				if ($errors[$i] instanceof Exception) {
+			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) 
+			{
+				if ($errors[$i] instanceof Exception) 
+				{
 					$app->enqueueMessage($errors[$i]->getMessage(), 'error');
-				} else {
+				} 
+				else 
+				{
 					$app->enqueueMessage($errors[$i], 'error');
 				}
 			}
-
+	
 			// Save the data in the session.
-			$app->setUserState('com_confmgt.edit.login.data', JFactory::getApplication()->input->get('jform', array(), 'array'));
-
+			$app->setUserState('com_confmgr.edit.loginform.data', JFactory::getApplication()->input->get('jform', array(), 'array'));
+	
 			// Redirect back to the edit screen.
-			$id = (int) $app->getUserState('com_confmgt.edit.login.id');
-			$this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=loginform&layout=edit&id='.$id, false));
+			$id = (int) $app->getUserState('com_confmgr.edit.loginform.id');
+			$this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=login&layout=default&id='.$id, false));
 			return false;
 		}
-	   
-	   jimport( 'joomla.user.authentication');
-       $auth = & JAuthentication::getInstance();
-       $credentials = array( 'username' => $data['username'], 'password' => $data['pw'] );
-       $options = array();
-       $response = $auth->authenticate($credentials, $options);
-
-       if ($response->status != JAUTHENTICATE_STATUS_SUCCESS) {
-		   $app->enqueueMessage(JText::_('Login falied, please try again with correct credentials'), 'error'); 
-		  $this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=loginform', false));
-	   }else{
-		   $app->login($credentials);
-		   $this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=entrypage', false));
-	   }
+	
+		jimport( 'joomla.user.authentication');
+		$auth = & JAuthentication::getInstance();
+		$credentials = array( 'username' => $data['username'], 'password' => $data['pw'] );
+		$options = array();
+		$response = $auth->authenticate($credentials, $options);
+	
+		if ($response->status != JAuthentication::STATUS_SUCCESS) 
+		{
+			$app->enqueueMessage(JText::_('COM_CONFMGR_CONTROLLER_ENTRYPAGE_LOGIN_FAILED'), 'error');
+			$this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=loginform', false));
+		}
+		else
+		{
+			$app->login($credentials);
+			$this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=entrypage', false));
+		}
 	}
  
     public function cancel() {
 		$this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=entrypage', false)); 
 	}
-    
-    
 }
