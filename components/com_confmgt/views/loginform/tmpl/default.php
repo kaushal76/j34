6510a3e0aa4370abac54baf3020a7b0eefe73728
kaushal76 <kaushal.keraminiyage@gmail.com>
2016-08-25 22:27:39 +0100

    Login Modal implementation attempt

--- components/com_confmgt/views/loginform/tmpl/default.php
+++ components/com_confmgt/views/loginform/tmpl/default.php
@@ -13,33 +13,7 @@ defined('_JEXEC') or die;
  * TODO 
  * Adjust the login box width and make it more aesthetically pleasing 
  */
+echo $this->loadTemplate('modal');
 
 ?>
 
-  <div class = 'form-horizontal'>
-    <fieldset>
-    <legend><h2><?php echo JText::_('LOGIN'); ?></h2></legend>
-      <div class="reg-edit front-end-edit">
-        <form id="form-reg" role="form" action="<?php echo JRoute::_('index.php'); ?>" method="post" class="form-validate form-horizontal" enctype="multipart/form-data">
-          <div class="control-group">
-            <?php echo $this->form->getLabel('username'); ?>
-            <div class="controls"> <?php echo $this->form->getInput('username'); ?> </div>
-          </div>
-          <div class="contrl-group">
-            <?php echo $this->form->getLabel('pw'); ?>
-            <div class="controls"> <?php echo $this->form->getInput('pw'); ?> </div>
-          </div>
-          <div class="control-group">
-            <div class="form-actions">
-              <button type="submit" class="btn btn-primary"><?php echo JText::_('LOGIN'); ?> </button>
-              <?php echo JText::_('or'); ?> <a href="<?php echo JRoute::_('index.php?option=com_confmgt&task=loginform.cancel'); ?>" title="<?php echo JText::_('JCANCEL'); ?> 
-				"><?php echo JText::_('JCANCEL'); ?> </a>
-              <input type="hidden" name="option" value="com_confmgt"/>
-              <input type="hidden" name="task" value="loginform.login"/>
-              <?php echo JHtml::_('form.token'); ?> 
-             </div>
-          </div>
-        </form>
-      </div>
-    </fieldset>
-  </div>
