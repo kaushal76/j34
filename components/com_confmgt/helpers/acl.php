d2919063372785e3405eccaef0d0997e96b6a345
Kausha Keraminiyage <keraminiyage@yahoo.co.uk>
2017-08-24 13:20:21 +0100

    Code clearing update to v 3.8.0

--- components/com_confmgt/helpers/acl.php
+++ components/com_confmgt/helpers/acl.php
@@ -10,7 +10,9 @@
 defined('_JEXEC') or die;
 
 /**
+ * @since version 3.8.0
  * ACL helper class for the confmgt. Using a specific ACL for the component as the Joomla ACL does not serve the purpose
+ * This class has methods to check authorisations and retrieve commonly used datasets
  */
 abstract class AclHelper
 {
@@ -111,12 +113,8 @@ abstract class AclHelper
      *
      * @since version 3.8.0
      */
-    public static function isStudentCoordinator($paperid = 0)
+    public static function isStudentCoordinator()
     {
-        //Check if student papers to be managed by the main theme leaders
-        $app = JFactory::getApplication();
-        $params = $app->getParams();
-        $student_papers_managed_by = $params->get('student_papers_managed_by', 1);
 
         //Obtain a database connection
         $db = JFactory::getDbo();
@@ -124,28 +122,6 @@ abstract class AclHelper
 
         //Build the query 
         $query = $db->getQuery(true);
-
-        //@TODO validate for each theme as theme leader
-        if ($student_papers_managed_by == 1) {
-            if ($paperid > 0) {
-                $query = $db->getQuery(true)->select('userid')->from($db->quoteName('#__confmgt_themes'))->where('userid = ' . (int)$user->id);
-            }
-            $query = $db->getQuery(true)->select('userid')->from($db->quoteName('#__confmgt_themes'))->where('userid = ' . (int)$user->id);
-            $db->setQuery($query);
-            $db->execute();
-            $num_rows = $db->getNumRows();
-            if ($num_rows > 0) {
-                return true;
-            } //$num_rows > 0
-            else {
-                return false;
-            }
-        }
-
-
-        if ($user->id == 0) {
-            return false;
-        }
 
         if (self::isSuperCoordinator()) {
             return true;
@@ -158,19 +134,26 @@ abstract class AclHelper
         $query->from($db->quoteName('#__confmgt_coordinators', 'a'));
         $query->where('a.userid = ' . (int)$user->id);
         $query->where('a.themeid = 0');
+
         // get the number of records
         $db->setQuery($query);
         $db->execute();
         $num_rows = $db->getNumRows();
         if ($num_rows > 0) {
             return true;
-        } //$num_rows > 0
+        }
         else {
             return false;
         }
     }
 
-    // function to check if the logged in user is a Super Coordinator
+    /**
+     * Function to check if the logged in user is a Super Coordinator
+     * @return bool
+     *
+     * @since version 3.8.0
+     */
+
     public static function isSuperCoordinator()
     {
         //Obtain a database connection
@@ -192,31 +175,42 @@ abstract class AclHelper
         $query->from($db->quoteName('#__confmgt_coordinators', 'a'));
         $query->where('a.userid = ' . (int)$user->id);
         $query->where('a.themeid = -1');
+
         // get the number of records
         $db->setQuery($query);
         $db->execute();
         $num_rows = $db->getNumRows();
         if ($num_rows > 0) {
             return true;
-        } //$num_rows > 0
+        }
         else {
             return false;
         }
     }
 
-    public static function isStudentPaper($paperid)
+    /**
+     * Function to check if a given paper is a student submission
+     * @param int $paperid
+     *
+     * @return bool
+     *
+     * @since version 3.8.0
+     */
+
+    public static function isStudentPaper($paperid = 0)
     {
         //Obtain a database connection
         $db = JFactory::getDbo();
         $user = JFactory::getUser();
         if ($user->id == 0) {
             return false;
-        } //$user->id == 0
+        }
 
-        $query = $db->getQuery(true)->select($db->quoteName(array(
-            'a.id',
-            'a.student_submission'
-        )))->from($db->quoteName('#__confmgt_papers', 'a'));
+        $query = $db->getQuery(true)
+            ->select($db->quoteName(array('a.id','a.student_submission')))
+            ->from($db->quoteName('#__confmgt_papers', 'a'))
+            ->where('a.id ='.(int)($paperid))
+            ->where('a.student_submission =0');
 
         // get the number of records
         $db->setQuery($query);
@@ -224,12 +218,20 @@ abstract class AclHelper
         $num_rows = $db->getNumRows();
         if ($num_rows > 0) {
             return true;
-        } //$num_rows > 0
+        }
         else {
             return false;
         }
     }
 
+    /**
+     * Function to check if the logged in user is a reviewer generally (paperid = 0) or for a given paper (paperid >0)
+     * @param int $paperid
+     *
+     * @return bool
+     *
+     * @since version 3.8.0
+     */
     public static function isRev1ewer($paperid = 0)
     {
         //Obtain a database connection
@@ -237,24 +239,25 @@ abstract class AclHelper
         $user = JFactory::getUser();
         if ($user->id == 0) {
             return false;
-        } //$user->id == 0
+        }
         if ($paperid == 0) {
             //Build the query 
-            $query = $db->getQuery(true)->select($db->quoteName(array(
-                'a.id',
-                'a.userid',
-                'a.agreed'
-            )))->from($db->quoteName('#__confmgt_rev1ewers', 'a'))->where('a.userid = ' . (int)$user->id)->where('a.agreed = 1');
-        } //$paperid == 0
+            $query = $db->getQuery(true)
+                ->select($db->quoteName(array('a.id', 'a.userid', 'a.agreed')))
+                ->from($db->quoteName('#__confmgt_rev1ewers', 'a'))
+                ->where('a.userid = ' . (int)$user->id)
+                ->where('a.agreed = 1');
+        }
         else {
             //Build the query 
-            $query = $db->getQuery(true)->select($db->quoteName(array(
-                'a.reviewerid',
-                'a.paperid',
-                'b.agreed',
-                'b.userid',
-                'c.id'
-            )))->from($db->quoteName('#__confmgt_rev1ewers_papers', 'a'))->join('INNER', $db->quoteName('#__confmgt_rev1ewers', 'b') . ' ON (' . $db->quoteName('a.reviewerid') . ' = ' . $db->quoteName('b.id') . ')')->join('INNER', $db->quoteName('#__users', 'c') . ' ON (' . $db->quoteName('b.userid') . ' = ' . $db->quoteName('c.id') . ')')->where('c.id = ' . (int)$user->id)->where('a.paperid = ' . (int)$paperid)->where('b.agreed = 1');
+            $query = $db->getQuery(true)
+                ->select($db->quoteName(array('a.reviewerid', 'a.paperid', 'b.agreed', 'b.userid', 'c.id')))
+                ->from($db->quoteName('#__confmgt_rev1ewers_papers', 'a'))
+                ->join('INNER', $db->quoteName('#__confmgt_rev1ewers', 'b') . ' ON (' . $db->quoteName('a.reviewerid') . ' = ' . $db->quoteName('b.id') . ')')
+                ->join('INNER', $db->quoteName('#__users', 'c') . ' ON (' . $db->quoteName('b.userid') . ' = ' . $db->quoteName('c.id') . ')')
+                ->where('c.id = ' . (int)$user->id)
+                ->where('a.paperid = ' . (int)$paperid)
+                ->where('b.agreed = 1');
         }
         // get the number of records
         $db->setQuery($query);
@@ -262,125 +265,148 @@ abstract class AclHelper
         $num_rows = $db->getNumRows();
         if ($num_rows > 0) {
             return true;
-        } //$num_rows > 0
+        }
         else {
             return false;
         }
     }
 
+    /**
+     * Function to get the theme for a given paper
+     * @param int $paperid
+     *
+     * @return bool|mixed
+     *
+     * @since version 3.8.0
+     */
+
     public static function getTheme($paperid = 0)
     {
+        if ($paperid == 0)
+        {
+            return false;
+        }
         //Obtain a database connection
         $db = JFactory::getDbo();
-        $user = JFactory::getUser();
 
-        //- paper id given, hence checking if the themeleader for the given paper
         //Build the query
         $query = $db->getQuery(true);
-        $query->select($db->quoteName(array(
-            'a.theme',
-            'a.id',
-            'b.userid',
-            'b.id',
-            'b.title'
-        )));
+        $query->select($db->quoteName(array('a.theme', 'a.id', 'b.userid', 'b.id', 'b.title')));
         $query->from($db->quoteName('#__confmgt_papers', 'a'));
         $query->join('INNER', $db->quoteName('#__confmgt_themes', 'b') . ' ON (' . $db->quoteName('a.theme') . ' = ' . $db->quoteName('b.id') . ')');
-        if ($paperid) {
-            $query->where('a.id = ' . (int)$paperid);
-        } //$paperid
+        $query->where('a.id = ' . (int)$paperid);
 
-        // get the number of records
         $db->setQuery($query);
         $row = $db->loadObject();
         if ($row) {
             return $row;
-        } //$row
+        }
         else {
             return false;
         }
     }
 
-    // function to get userid by entering the email
+    /**
+     * Function to get userid by entering the email
+     * @param $email
+     *
+     * @return bool
+     *
+     * @since version 3.8.0
+     */
     public static function getUserID($email)
     {
         $db = JFactory::getDbo();
-        $user = JFactory::getUser();
-        $query = $db->getQuery(true)->select('id, email')->from($db->quoteName('#__users'))->where($db->quoteName('email') . ' = ' . $db->quote($email));
+        $query = $db->getQuery(true)
+            ->select('id, email')
+            ->from($db->quoteName('#__users'))
+            ->where($db->quoteName('email') . ' = ' . $db->quote($email));
         //Prepare the query
         $db->setQuery($query);
         // Load the row.
         $row = $db->loadObject();
         if ($row) {
             return $row->id;
-        } //$row
+        }
         else {
             return false;
         }
     }
 
-    //Function to get Reviewer ID by email
+    /**
+     * Function to get Reviewer ID by email
+     * @param $email
+     *
+     * @return bool|mixed
+     *
+     * @since version 3.8.0
+     */
     public static function getRev1ewerID($email)
     {
         //Obtain a database connection
         $db = JFactory::getDbo();
-        $user = JFactory::getUser();
-        //Build the query 
-        $query = $db->getQuery(true)->select($db->quoteName(array(
-            'a.id',
-            'a.userid',
-            'a.email',
-            'a.agreed'
-        )))->from($db->quoteName('#__confmgt_rev1ewers', 'a'))->where($db->quoteName('email') . ' = ' . $db->quote($email));
+        $query = $db->getQuery(true)->select($db->quoteName(array('a.id', 'a.userid', 'a.email', 'a.agreed')))
+            ->from($db->quoteName('#__confmgt_rev1ewers', 'a'))
+            ->where($db->quoteName('email') . ' = ' . $db->quote($email));
         //Prepare the query
         $db->setQuery($query);
         $row = $db->loadObject();
         if ($row) {
             return $row;
-        } //$row
+        }
         else {
             return false;
         }
     }
 
-    //Function to get the number of reviewers for a given paper
+    /**
+     * Function to get the number of reviewers for a given paper
+     * @param $paperid
+     *
+     * @return bool|int
+     *
+     * @since version 3.8.0
+     */
     public static function getNumofRev1ewers($paperid)
     {
         //Obtain a database connection
         $db = JFactory::getDbo();
-        $user = JFactory::getUser();
-        if ($user->id == 0) {
-            return false;
-        } //$user->id == 0
-        //Build the query 
-        $query = $db->getQuery(true)->select($db->quoteName(array(
-            'a.id',
-            'a.userid',
-            'a.email',
-            'a.agreed'
-        )))->from($db->quoteName('#__confmgt_rev1ewers_papers', 'a'))->where($db->quoteName('paperid') . ' = ' . (int)($paperid));
+        $query = $db->getQuery(true)
+            ->select($db->quoteName(array('a.id', 'a.userid', 'a.email', 'a.agreed')))
+            ->from($db->quoteName('#__confmgt_rev1ewers_papers', 'a'))
+            ->where($db->quoteName('paperid') . ' = ' . (int)($paperid));
         //Prepare the query
         $db->setQuery($query);
         $db->execute();
         $num_rows = $db->getNumRows();
         if ($num_rows > 0) {
             return $num_rows;
-        } //$num_rows > 0
+        }
         else {
             return false;
         }
     }
 
+    /**
+     * Get authors for a given paper
+     * @param int $paperid
+     *
+     * @return bool|mixed
+     *
+     * @since version 3.8.0
+     */
     public static function getAuthors($paperid = 0)
     {
+        if ($paperid ==0)
+        {
+            return false;
+        }
+
         //Obtain a database connection
         $db = JFactory::getDbo();
-        $user = JFactory::getUser();
-
-        //- paper id given, hence checking if the themeleader for the given paper
-        //Build the query
         $query = $db->getQuery(true);
-        $query->select($db->quoteName(array(
+        $query
+            ->select($db->quoteName(array(
             'a.title',
             'a.firstname',
             'a.surname',
@@ -391,17 +417,15 @@ abstract class AclHelper
             'a.ordering'
         )));
         $query->from($db->quoteName('#__confmgt_authors', 'a'));
-        if ($paperid) {
-            $query->where('a.linkid = ' . (int)$paperid);
-            $query->order('a.ordering ASC');
-        } //$paperid
+        $query->where('a.linkid = ' . (int)$paperid);
+        $query->order('a.ordering ASC');
 
         // get the number of records
         $db->setQuery($query);
         $rows = $db->loadObjectList();
         if ($rows) {
             return $rows;
-        } //$row
+        }
         else {
             return false;
         }
