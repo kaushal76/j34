1596df52bb0f4f8b479f706e5ddc3d4cf6246c9c
keraminiyage <kaushal.keraminiyage@gmail.com>
2017-09-14 19:57:35 +0100

    Code clean up updating to v 3.8.0

--- components/com_confmgt/views/paper/tmpl/default_information.php
+++ components/com_confmgt/views/paper/tmpl/default_information.php
@@ -1,57 +1,59 @@
 <?php
 /**
- * @version     2.5.7
+ * @version     3.8.0
  * @package     com_confmgt
- * @copyright   Copyright (C) 2015. All rights reserved.
+ * @copyright   Copyright (C) 2017. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  * @author      Dr Kaushal Keraminiyage <admin@confmgt.com> - htttp://www.confmgt.com
  */
 // no direct access
 
 defined('_JEXEC') or die;
-
-$url = "components/com_confmgt/assets/js/view.paper.information.js";
-$document = JFactory::getDocument();
-$document->addScript($url);
-
 ?>
+
 <div class="panel panel-default">
-<h1><?php echo JText::_('General Information'); ?></h1> 
-<div>
-  <table class="table table-striped">
-    <tr>
-      <td class="header" style="width:30%"><?php
-		echo JText::_('COM_CONFMGT_FORM_LBL_PAPER_ID'); ?>
-        : </td>
-      <td><?php echo $this->item->id; ?></td>
-      <td style="width:15%">&nbsp;</td>
-    </tr>
-    <tr>
-      <td class="header"><?php
-		echo JText::_('COM_CONFMGT_FORM_LBL_PAPER_TITLE'); ?>
-        :
-      </td>
-      <td>
-      <div 
-      class = "edit" 
-      id="title" 
-      data-type="text" 
-      data-pk="<?php echo $this->item->id; ?>" 
-      data-url="<?php echo JRoute::_('index.php?option=com_confmgt&task=paperform.save_ajax&tmpl=component&format=raw'); ?>" 
-      data-title="Enter Title" 
-      data-name = "title">
-	  <?php echo $this->item->title; ?>
-      </div>
-      </td>
-      <td><button class="btn edit_title" type="submit"><i class="icon-edit"></i></button></td> 
-    </tr>
-     <tr>
-      <td class="header"><?php
-		echo JText::_('COM_CONFMGT_FORM_LBL_PAPER_CREATED_BY'); ?>
-        :</td>
-      <td><?php echo $this->item->created_by; ?></td>
-      <td style="width:15%">&nbsp;</td>
-    </tr>
-  </table>
-</div>
+    <div class="panel-heading">
+        <h1><?php echo JText::_('Abstract'); ?></h1>
+    </div>
+        <table class="table table-striped">
+            <tr>
+                <td class="header" style="width:30%"><?php
+                    echo JText::_('Paper ID: '); ?>
+                </td>
+                <td><?php echo $this->item->id; ?></td>
+            </tr>
+            <tr>
+                <td class="header"><?php
+                    echo JText::_('Paper Title: '); ?>
+                </td>
+                <td>
+                        <?php echo $this->item->title; ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="header"><?php
+                    echo JText::_('Submitted by: '); ?>
+                </td>
+                <td><?php echo $this->item->created_by; ?></td>
+            </tr>
+            <tr>
+                <td class="header"><?php
+                    echo JText::_('Absrtact: '); ?>
+                </td>
+                <td><?php echo $this->item->abstract; ?></td>
+            </tr>
+            <tr>
+                <td class="header"><?php
+                    echo JText::_('Keywords: '); ?>
+                </td>
+                <td><?php echo $this->item->keywords; ?></td>
+            </tr>
+        </table>
+    <div class="panel-footer">
+        <a class="btn btn-default"
+            href="<?php echo JRoute::_('index.php?option=com_confmgt&linkid='.$this->item->id.'&task=paper.edit&id=' . $this->item->id);  ?>">
+            <i class="icon-pencil"></i>
+            <?php echo JText::_('Edit'); ?>
+        </a>
+    </div>
 </div>
